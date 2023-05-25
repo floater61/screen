1b16816d5f8de10a0302118da8e0ccdbedd0dd78
floater61 <134603723+floater61@users.noreply.github.com>
2023-05-25 11:29:52 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        # . . . #
+        `)
+})
 basic.clearScreen()
 basic.forever(function () {
     basic.showString("Hello!")
